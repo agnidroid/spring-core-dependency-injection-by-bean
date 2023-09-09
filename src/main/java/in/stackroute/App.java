f0536349a90f31465fc65ead74aa9bcaa9fd4f08
agnidroid <agni7601083@gmail.com>
2023-09-10 02:57:03 +0530

    Creating spring beans using java annotation configuration

--- src/main/java/in/stackroute/App.java
+++ src/main/java/in/stackroute/App.java
@@ -12,7 +12,7 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		Course course=ctx.getBean("course",Course.class);
-//		Student student=ctx.getBean("student",Student.class);
+		Student student=ctx.getBean("student",Student.class);
 //		Professor professor=ctx.getBean("professor",Professor.class);
 		course.setProfessor( ctx.getBean("professor",Professor.class));
 
